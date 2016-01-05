c9221d20cc45e8b27daf1204517b8deaeecbb8c3
wekey123 <mail2rmvignesh@gmail.com>
2016-01-05 18:09:33 +0530

    If Image not availble replace with default image

--- app/webroot/js/angular/directives.js
+++ app/webroot/js/angular/directives.js
@@ -58,3 +58,22 @@ shopping.directive('asidemenuDirective', function() {
 	}
     
 });
+
+shopping.directive('errSrc', function() {
+	console.log('errSrc');
+      return {
+        link: function(scope, element, attrs) {
+          element.bind('error', function() {
+            if (attrs.src != attrs.errSrc) {
+              attrs.$set('src', attrs.errSrc);
+            }
+          });
+          
+          attrs.$observe('ngSrc', function(value) {
+            if (!value && attrs.errSrc) {
+              attrs.$set('src', attrs.errSrc);
+            }
+          });
+        }
+      }
+    });
