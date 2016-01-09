85b469d7a8d5c563cfcbb8d9fa37b1bec3a356e6
vignesh <mail2rmvignesh@gmail.com>
2016-01-09 10:44:36 +0530

    Json For Registration Service

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -300,5 +300,23 @@ class UsersController extends AppController {
 		$this->set(array('userLogin' => $responseLogin,'_serialize' => array('userLogin')));
 	}
 
+	public function registration() {
+		$this->layout = ''; 
+		header("Access-Control-Allow-Origin: *");
+		header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+		if ($this->request->is('post')) {
+			$this->User->create($this->request->data);
+			if ($this->User->save($this->request->data)) {
+				$this->Flash->success(__('The user has been saved.'));
+				return $this->redirect(array('action' => 'index'));
+			} else {
+				$responseRegistration = array('message'=>'The user could not be saved. Please, try again.','Response'=>'E');
+			}
+		}
+		$this->set(array('userRegistration' => $responseRegistration,'_serialize' => array('userRegistration')));
+	}
+	
+
+		
 
 }
